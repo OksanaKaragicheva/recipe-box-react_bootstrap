d2e53cd8a151760d8bd0f39817be1db8150ea636
Oksana <okkaragicheva@gmail.com>
2019-02-07 08:46:27 +0200

    modified App.js file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faTimes, faPencilAlt, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import AddRecipe from './Components/AddRecipe';
 import './App.sass';
-import './App.sass';
 
 library.add(faTimes, faPencilAlt, faTrashAlt);
 
